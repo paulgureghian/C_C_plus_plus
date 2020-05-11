244de3f325a8ba56049c5eb681ca480af6f67bce
paulgureghian <paulgureghian@att.net>
2020-05-11 12:26:00 -0700

    Added code for Hello_World

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -1,1 +1,14 @@
-// Created by Paul A. Gureghian. //
+// Created by Paul A. Gureghian in May 2020. //
+// This C++ program prints "Hello World" to the console. //
+
+// Start of program. //
+
+// Import libraries. //
+# include <iostream>
+using namespace std;
+
+// Define main(). //
+int main()
+{
+    cout << "Hello World" << endl;
+}
