9c45f6f916c24d2dd409595c441f4e1bdb73eecb
paulgureghian <paulgureghian@att.net>
2020-06-12 16:30:01 -0700

    Added a 2D vector

---
+++ Grid.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::vector;
+
+int main() {
+
+    vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
+                                {0, 1, 0, 0, 0, 0},
+                                {0, 1, 0, 0, 0, 0},
+                                {0, 1, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 1, 0}};
+
+
+}
