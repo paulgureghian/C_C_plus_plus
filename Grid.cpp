9b5d580b561c740bda99aa99945d7bb5bc015bd4
paulgureghian <paulgureghian@att.net>
2020-06-13 16:47:13 -0700

    Added a function

--- Grid.cpp
+++ Grid.cpp
@@ -4,6 +4,15 @@
 using std::cout;
 using std::vector;
 
+void PrintBoard(const vector<vector<int>> board) {
+  for (int i =0; i < board.size(); i++) {
+    for (int j =0; j < board[i].size(); j++) {
+        cout << board[i][j];
+    }  
+    cout << "\n";
+  }
+}
+
 int main() {
 
     vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
@@ -12,5 +21,5 @@ int main() {
                                 {0, 1, 0, 0, 0, 0},
                                 {0, 0, 0, 0, 1, 0}};
     
-    cout << "Task completed successfully" << "\n";
+    PrintBoard(board);
 }
