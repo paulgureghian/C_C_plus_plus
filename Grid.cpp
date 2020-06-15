55b5b72e7d23c58c0c04ee6d72ab51ea39d581a9
paulgureghian <paulgureghian@att.net>
2020-06-15 16:40:05 -0700

    Updated the functions

--- Grid.cpp
+++ Grid.cpp
@@ -12,24 +12,29 @@ using std::vector;
 
 enum class State {kEmpty, kObstacle};
 
-vector<int> ParseLine(string line) {
+vector<State> ParseLine(string line) {
     istringstream sline(line);
     int n;
     char c;
-    vector<int> row;
+    vector<State> row;
     while (sline >> n >> c && c == ',') {
-      row.push_back(n);
-    }
+      if (n == 0) {
+        row.push_back(State::kEmpty);
+      } else {
+        row.push_back(State::kObstacle);
+      }
+    }  
+    
     return row;     
 }
 
-vector<vector<int>> ReadBoardFile(string path) {
+vector<vector<State>> ReadBoardFile(string path) {
   ifstream myfile (path);
-  vector<vector<int>> board{};
+  vector<vector<State>> board{};
   if (myfile) {
     string line;
     while (getline(myfile, line)) {
-      vector<int> row = ParseLine(line);
+      vector<State> row = ParseLine(line);
       board.push_back(row);
     }
   }
@@ -43,10 +48,10 @@ string CellString(State cell) {
   }
 }
 
-void PrintBoard(const vector<vector<int>> board) {
+void PrintBoard(const vector<vector<State>> board) {
   for (int i =0; i < board.size(); i++) {
     for (int j =0; j < board[i].size(); j++) {
-        cout << board[i][j];
+        cout << CellString(board[i][j]);
     }  
     cout << "\n";
   }
