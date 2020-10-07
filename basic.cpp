9dbd2bc8c5a9f35af34e42bdbae5e60fe05f17f4
PaulGureghian <paulgureghian@att.net>
2020-10-06 17:26:52 -0700

    Added basic.cpp source file

---
+++ basic.cpp
@@ -0,0 +1,16 @@
+// Created by Paul A. Gureghian in Oct 2020. 
+// A basic C++ program. 
+
+// Header file
+#include <iostream> 
+
+// Link the standard C++ library
+using namespace std;
+
+// Define main function
+int main(){
+
+    cout << "I am a C++ programmer\n";
+
+    return 0; 
+}
